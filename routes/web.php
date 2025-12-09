761d177fda0031522ea47f80f5e3f7c2edab592a
itsloraee <laurxee@gmail.com>
2025-12-09 11:11:39 -0400

    Layout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
@@ -29,8 +30,6 @@ Route::middleware(['auth'])->group(function () {
 require __DIR__.'/auth.php';
 
 /*
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,12 +38,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+*/
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
-*/
